4914a7b249bffad5e62ede53b89d17665ca4c362
Anmol <89922277+jinckzx@users.noreply.github.com>
2024-11-20 23:39:21 +0530

    spotify.ts

--- src/config/spotify.ts
+++ src/config/spotify.ts
@@ -1,6 +1,6 @@
 export const SPOTIFY_CONFIG = {
   CLIENT_ID: '8ca86e34b0e2415d880110ef59f756c8', // Add your Spotify Client ID here
-  REDIRECT_URI: 'https://musicify-avyiy60hk-jinckzxs-projects.vercel.app/',
+  REDIRECT_URI: 'https://musicify-one.vercel.app/callback',
   SCOPES: [
     'user-read-private',
     'user-read-email',
